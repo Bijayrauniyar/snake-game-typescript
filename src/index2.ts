a9999ae8edb4bfd61db80e7ed8ddf7b0587051e7
Bijayrauniyar <rauniyarbijay117@gmail.com>
2020-02-05 11:10:40 +0545

    Spiliting code with classes

---
+++ src/index2.ts
@@ -0,0 +1,146 @@
+import { Canvas } from './canvas.js';
+import { Keys, Colors } from './constants.js';
+
+let gameCanvas: HTMLCanvasElement;
+
+interface Point {
+  x: number;
+  y: number;
+}
+let snake: Array<Point>;
+
+// Horizontal velocity
+let dx: number = 10;
+// Vertical velocity
+let dy: number = 0;
+let changingDirection: boolean = false;
+// The user's score
+let score: number = 0;
+let foodX: number;
+let foodY: number;
+let gameSpeed: number = 100;
+
+// Get the canvas element
+gameCanvas = document.getElementById('gameCanvas') as HTMLCanvasElement;
+
+// change direction on keypress
+document.addEventListener('keydown', changeDirection);
+
+snake = [
+  { x: 150, y: 150 },
+  { x: 140, y: 150 },
+  { x: 130, y: 150 },
+  { x: 120, y: 150 },
+  { x: 110, y: 150 }
+];
+
+// create board & canvas
+const canvas = new Canvas(300, 300, 0, 0);
+canvas.init(gameCanvas);
+
+gameLoop();
+createFood();
+
+function gameLoop() {
+  if (didGameEnd()) return;
+  setTimeout(function onTick() {
+    changingDirection = false;
+    canvas.clearCanvas();
+    drawFood();
+    moveSnake();
+    drawSnake();
+    // Call gameLoop again
+    gameLoop();
+  }, gameSpeed);
+}
+
+/**
+ * moving the snake by changing the x-coordinates of its parts
+ * according to the horizontal velocity and the y-coordinates of its parts
+ * according to the vertical veolocity
+ */
+function moveSnake() {
+  const head = { x: snake[0].x + dx, y: snake[0].y + dy };
+  snake.unshift(head);
+  const didEatFood = snake[0].x === foodX && snake[0].y === foodY;
+  if (didEatFood) {
+    score += 10;
+    document.getElementById('score')!.innerHTML = score.toString();
+    createFood();
+  } else {
+    snake.pop();
+  }
+}
+
+// draw the snake on the canvas
+function drawSnakePart(snakePart: { x: number; y: number }) {
+  Canvas.context.fillStyle = Colors.SNAKE_COLOUR;
+  Canvas.context.strokeStyle = Colors.SNAKE_BORDER_COLOUR;
+  Canvas.context.fillRect(snakePart.x, snakePart.y, 10, 10);
+  Canvas.context.strokeRect(snakePart.x, snakePart.y, 10, 10);
+}
+
+function drawSnake() {
+  snake.map(snakePart => drawSnakePart(snakePart));
+}
+
+// food
+
+function randomTen(min: number, max: number) {
+  return Math.round((Math.random() * (max - min) + min) / 10) * 10;
+}
+function createFood() {
+  foodX = randomTen(0, gameCanvas.width - 10);
+  foodY = randomTen(0, gameCanvas.height - 10);
+
+  snake.forEach(part => {
+    const foodIsOnSnake = part.x == foodX && part.y == foodY;
+    if (foodIsOnSnake) createFood();
+  });
+}
+function drawFood() {
+  Canvas.context.fillStyle = Colors.FOOD_COLOUR;
+  Canvas.context.strokeStyle = Colors.FOOD_BORDER_COLOUR;
+  Canvas.context.fillRect(foodX, foodY, 10, 10);
+  Canvas.context.strokeRect(foodX, foodY, 10, 10);
+}
+
+// End the game
+function didGameEnd() {
+  for (let i = 4; i < snake.length; i++) {
+    const didCollide = snake[i].x === snake[0].x && snake[i].y === snake[0].y;
+    if (didCollide) return true;
+  }
+  const hitLeftWall = snake[0].x < 0;
+  const hitRightWall = snake[0].x > gameCanvas.width - 10;
+  const hitToptWall = snake[0].y < 0;
+  const hitBottomWall = snake[0].y > gameCanvas.height - 10;
+  return hitLeftWall || hitRightWall || hitToptWall || hitBottomWall;
+}
+
+function changeDirection(event: KeyboardEvent) {
+  if (changingDirection) return;
+  changingDirection = true;
+
+  const keyPressed = event.keyCode;
+  const goingUp = dy === -10;
+  const goingDown = dy === 10;
+  const goingRight = dx === 10;
+  const goingLeft = dx === -10;
+  if (keyPressed === Keys.LEFT_KEY && !goingRight) {
+    dx = -10;
+    dy = 0;
+  }
+  if (keyPressed === Keys.UP_KEY && !goingDown) {
+    dx = 0;
+    dy = -10;
+  }
+  if (keyPressed === Keys.RIGHT_KEY && !goingLeft) {
+    dx = 10;
+    dy = 0;
+  }
+  if (keyPressed === Keys.DOWN_KEY && !goingUp) {
+    dx = 0;
+    dy = 10;
+  }
+}
